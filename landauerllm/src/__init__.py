9c360e5d4ac7877ec7285be572b0f05da72fc32c
Jeff <124529638+Jffrsntaylor@users.noreply.github.com>
2025-12-10 01:39:22 -0800

    Delete landauerllm directory

--- landauerllm/src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Package entrypoint."""
-
-from .landauer_llm import AperiodicRNN, LandauerLanguageModel, ThermodynamicInertiaLoss
-
-__all__ = ["AperiodicRNN", "LandauerLanguageModel", "ThermodynamicInertiaLoss"]
